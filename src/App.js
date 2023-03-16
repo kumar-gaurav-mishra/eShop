126b580610c9ee7245851c2287aab55f7bf397f7
Kumar Gaurav Mishra <kumar@192.168.1.48>
2023-03-16 11:06:42 +0530

    partially done

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Checkout from "./components/Checkout";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route
+            path="/checkout"
+            element={
+              <>
+                <Header />
+                <Checkout />
+              </>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <Home />
+              </>
+            }
+          />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
